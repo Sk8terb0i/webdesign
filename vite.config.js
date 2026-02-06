6740314884f8224c0878f470f180866a3e4d2998
sk8terboi <noe.arnold@outlook.com>
2026-02-06 13:56:47 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/webdesign/", // YOUR REPOSITORY NAME HERE
+  base: "/", // YOUR REPOSITORY NAME HERE
 });
